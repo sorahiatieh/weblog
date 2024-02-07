22df5dc41f357ff9e61b8a5b8b163adf29a24e69
atieh <a.sorahitalab@gmail.com>
2024-02-07 13:29:08 +0330

    added news section

--- include/layout/sidebar.php
+++ include/layout/sidebar.php
@@ -37,17 +37,41 @@
         <div class="card-body">
             <p class="fw-bold fs-6">عضویت در خبرنامه</p>
 
-            <form>
+            <?php
+                $invalidInputName="";
+                $invalidInputEmail="";
+                $message="";
+
+                if(isset($_POST['subscribe'])){
+                    if(empty(trim($_POST['name']))){
+                        $invalidInputName="فیلد نام الزامیست!";
+                    }elseif (empty(trim($_POST['email']))){
+                        $invalidInputEmail="فیلد ایمیل الزامیست!";
+                    }else{
+                        $name=$_POST['name'];
+                        $email=$_POST['email'];
+
+                        $subscribeInsert=$db->prepare("INSERT INTO tbl_subscribers (name , email) VALUES (:name , :email)");
+                        $subscribeInsert->execute(['name' => $name , 'email' => $email]);
+
+                        $message="عضویت شما با موفقیت انجام شد!";
+                    }
+                }
+            ?>
+            <div class="text-success"><?= $message; ?></div>
+            <form method="post">
                 <div class="mb-3">
                     <label class="form-label">نام</label>
-                    <input type="text" class="form-control"/>
+                    <input type="text" name="name" class="form-control"/>
+                    <div class="form-text text-danger"><?= $invalidInputName; ?></div>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">ایمیل</label>
-                    <input type="email" class="form-control"/>
+                    <input type="email" name="email" class="form-control"/>
+                    <div class="form-text text-danger"><?= $invalidInputEmail; ?></div>
                 </div>
                 <div class="d-grid gap-2">
-                    <button type="submit" class="btn btn-secondary">ارسال</button>
+                    <button type="submit" name="subscribe" class="btn btn-secondary">ارسال</button>
                 </div>
             </form>
         </div>
