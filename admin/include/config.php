65a6340c5226c9b371d5ccd2d46595ebbd338c6a
atieh <a.sorahitalab@gmail.com>
2024-02-18 13:29:10 +0330

    create admin section

---
+++ admin/include/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define("DNS", "mysql:host=localhost;dbname=weblog;charset=utf8mb4");
+
+define("DB_USER", "root");
+
+define("DB_PASS", "");
---
+++ admin/include/db.php
@@ -0,0 +1,3 @@
+<?php
+
+$db = new PDO(DNS, DB_USER, DB_PASS);
