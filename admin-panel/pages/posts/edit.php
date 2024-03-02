59d3da293981062d84b7c89c486f62e835d7d3c9
atieh <a.sorahitalab@gmail.com>
2024-03-02 12:54:15 +0330

    update edit post

--- admin-panel/pages/posts/edit.php
+++ admin-panel/pages/posts/edit.php
@@ -1,18 +1,65 @@
 <?php
 include "../../include/layout/header.php";
 
-if(isset($_GET['id'])){
-    $postId=$_GET['id'];
-    $post=$db->prepare("SELECT * FROM tbl_posts WHERE id= :id");
-    $post->execute(['id' =>$postId]);
-    $post=$post->fetch();
+if (isset($_GET['id'])) {
+    $postId = $_GET['id'];
+    $post = $db->prepare('SELECT * FROM tbl_posts WHERE id = :id');
+    $post->execute(['id' => $postId]);
+    $post = $post->fetch();
 
     $categories = $db->query("SELECT * FROM tbl_categories");
 }
+
+$invalidInputTitle = '';
+$invalidInputAuthor = '';
+$invalidInputBody = '';
+
+if (isset($_POST['editPost'])) {
+
+    if (empty(trim($_POST['title']))) {
+        $invalidInputTitle = 'فیلد عنوان مقاله الزامیست';
+    }
+
+    if (empty(trim($_POST['author']))) {
+        $invalidInputAuthor = 'فیلد نویسنده مقاله الزامیست';
+    }
+
+    if (empty(trim($_POST['body']))) {
+        $invalidInputBody = 'فیلد متن مقاله الزامیست';
+    }
+
+    if (!empty(trim($_POST['title'])) && !empty(trim($_POST['author'])) && !empty(trim($_POST['body']))) {
+        $title = $_POST['title'];
+        $author = $_POST['author'];
+        $body = $_POST['body'];
+        $categoryId = $_POST['categoryId'];
+
+        if (!empty(trim($_FILES['image']['name']))) {
+            $nameImage = time() . "_" . $_FILES['image']['name'];
+            $tmpName = $_FILES['image']['tmp_name'];
+
+            if (move_uploaded_file($tmpName, "../../../uploads/posts/$nameImage")) {
+                $postUpdate = $db->prepare("UPDATE tbl_posts SET title =:title, author=:author, category_id=:categoryId, body=:body, image=:image WHERE id=:id");
+                $postUpdate->execute(['title' => $title, 'author' => $author, 'categoryId' => $categoryId, 'body' => $body, 'id' => $postId, 'image' => $nameImage]);
+            } else {
+                echo "Upload Error";
+            }
+        } else {
+            $postUpdate = $db->prepare("UPDATE tbl_posts SET title =:title, author=:author, category_id=:categoryId, body=:body WHERE id=:id");
+            $postUpdate->execute(['title' => $title, 'author' => $author, 'categoryId' => $categoryId, 'body' => $body, 'id' => $postId]);
+        }
+
+
+        header("Location:index.php");
+        exit();
+    }
+}
+
 ?>
 
     <div class="container-fluid">
         <div class="row">
+            <!-- Sidebar Section -->
             <?php
             include "../../include/layout/sidebar.php"
             ?>
@@ -25,36 +72,39 @@ if(isset($_GET['id'])){
 
                 <!-- Posts -->
                 <div class="mt-4">
-                    <form method="post" class="row g-4">
+                    <form method="post" class="row g-4" enctype="multipart/form-data">
                         <div class="col-12 col-sm-6 col-md-4">
                             <label class="form-label">عنوان مقاله</label>
-                            <input type="text" name="title" class="form-control" value="<?= $post['title'] ?>"/>
+                            <input type="text" name="title" class="form-control" value="<?= $post['title'] ?>" />
+                            <div class="form-text text-danger"><?= $invalidInputTitle ?></div>
                         </div>
 
                         <div class="col-12 col-sm-6 col-md-4">
                             <label class="form-label">نویسنده مقاله</label>
-                            <input type="text" name="author" class="form-control" value="<?= $post['author'] ?>"/>
+                            <input type="text" name="author" class="form-control" value="<?= $post['author'] ?>" />
+                            <div class="form-text text-danger"><?= $invalidInputAuthor ?></div>
                         </div>
 
                         <div class="col-12 col-sm-6 col-md-4">
                             <label class="form-label">دسته بندی مقاله</label>
                             <select name="categoryId" class="form-select">
-                                <?php if($categories->rowCount() >0):?>
-                                    <?php foreach ($categories as $item): ?>
-                                        <option <?= ($item['id'] == $post['category_id']) ? 'selected' : '' ?> value="<?= $item['id'] ?>"><?= $item['title'] ?></option>
-                                    <?php endforeach; ?>
-                                <?php endif; ?>
+                                <?php if ($categories->rowCount() > 0) : ?>
+                                    <?php foreach ($categories as $category) : ?>
+                                        <option <?= ($category['id'] == $post['category_id']) ? 'selected' : '' ?> value="<?= $category['id'] ?>"><?= $category['title'] ?></option>
+                                    <?php endforeach ?>
+                                <?php endif ?>
                             </select>
                         </div>
 
                         <div class="col-12 col-sm-6 col-md-4">
                             <label for="formFile" class="form-label">تصویر مقاله</label>
-                            <input class="form-control" name="image" type="file" />
+                            <input name="image" class="form-control" type="file" />
                         </div>
 
                         <div class="col-12">
                             <label for="formFile" class="form-label">متن مقاله</label>
-                            <textarea class="form-control" rows="8"><?= $post['body'] ?></textarea>
+                            <textarea name="body" class="form-control" rows="8"><?= $post['body'] ?></textarea>
+                            <div class="form-text text-danger"><?= $invalidInputBody ?></div>
                         </div>
 
 
