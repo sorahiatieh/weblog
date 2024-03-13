c146f9114e3779d8e45997dc303a50265a3c23b5
atieh <a.sorahitalab@gmail.com>
2024-03-13 10:38:34 +0330

    authentication login page

--- admin-panel/include/layout/header.php
+++ admin-panel/include/layout/header.php
@@ -14,7 +14,7 @@ $path = $_SERVER['REQUEST_URI'];
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>php tutorial || blog project || webprog.io</title>
+    <title>blog project</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
